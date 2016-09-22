daaeb130ea548282056ad37fae9d7c0aa6afc1e5
MyGvs <mygvs.mh@gmail.com>
2016-09-21 23:35:55 -0400

    agregando un mapa a la aplicacón

--- app/src/main/java/com/fuzzyapps/gustoboliviano/MainNavigationActivity.java
+++ app/src/main/java/com/fuzzyapps/gustoboliviano/MainNavigationActivity.java
@@ -1,13 +1,79 @@
 package com.fuzzyapps.gustoboliviano;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.FragmentManager;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.IdRes;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
-public class MainNavigationActivity extends AppCompatActivity {
+import it.sephiroth.android.library.bottomnavigation.BadgeProvider;
+import it.sephiroth.android.library.bottomnavigation.BottomNavigation;
 
+public class MainNavigationActivity extends BaseActivity implements BottomNavigation.OnMenuItemSelectionListener {
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        initializeBottomNavigation(savedInstanceState);
+        //Toast.makeText(getApplication(),"",)
+    }
+    protected void initializeBottomNavigation(final Bundle savedInstanceState) {
+        if (null == savedInstanceState) {
+            getBottomNavigation().setDefaultSelectedIndex(0);
+            changeToolbarColor(0);
+            final BadgeProvider provider = getBottomNavigation().getBadgeProvider();
+        }
+    }
+    @Override
+    public void onMenuItemSelect(@IdRes final int itemId, final int position) {
+        changeToolbarColor(position);
+        Toast.makeText(MainNavigationActivity.this,""+position,Toast.LENGTH_SHORT).show();
+    }
+
+    private void changeToolbarColor(int position) {
+        int primaryColor = 0;
+        int darkColor = 0;
+        FragmentManager fragmentManager = getFragmentManager();
+
+        switch (position){
+            case 0:
+                primaryColor = Color.parseColor("#1F1F1F");
+                darkColor = Color.parseColor("#000000");
+                break;
+            case 1:
+                primaryColor = Color.parseColor("#669900");
+                darkColor = Color.parseColor("#527a00");
+                break;
+            case 2:
+                primaryColor = Color.parseColor("#FF5252");
+                darkColor = Color.parseColor("#ff1515");
+                fragmentManager.beginTransaction()
+                        .replace(R.id.content_frame, new mapFragment())
+                        .commit();
+                break;
+            case 3:
+                primaryColor = Color.parseColor("#FF8800");
+                darkColor = Color.parseColor("#e07800");
+                break;
+            case 4:
+                primaryColor = Color.parseColor("#764ac6");
+                darkColor = Color.parseColor("#4d2b89");
+                break;
+        }
+        toolbar.setBackgroundColor(primaryColor);
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().setNavigationBarColor(primaryColor);
+            getWindow().setStatusBarColor(darkColor);
+        }
+    }
+
+    @Override
+    public void onMenuItemReselect(@IdRes final int itemId, final int position) {
+        Toast.makeText(MainNavigationActivity.this,"2. "+position,Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/fuzzyapps/gustoboliviano/mapFragment.java
@@ -0,0 +1,61 @@
+package com.fuzzyapps.gustoboliviano;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.design.widget.FloatingActionButton;
+import android.view.InflateException;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class mapFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private View view;
+
+    public mapFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        //view = inflater.inflate(R.layout.fragment_map, container, false);
+        if (view != null) {
+            ViewGroup parent = (ViewGroup) view.getParent();
+            if (parent != null)
+                parent.removeView(view);
+        }
+        try {
+            view = inflater.inflate(R.layout.fragment_map, container, false);
+        } catch (InflateException e) {
+            //Toast.makeText(getActivity(),""+e.getMessage(),Toast.LENGTH_SHORT).show();
+        }
+        MapFragment mapFragment = (MapFragment)  this.getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
