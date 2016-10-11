33bef546bd09b4113504b8163576e19d846f1221
MyGvs <mygvs.mh@gmail.com>
2016-10-10 23:15:28 -0400

    Interfaz donde el usuario puede realizar las criticas

--- app/src/main/java/com/fuzzyapps/gustoboliviano/profileFragment.java
+++ app/src/main/java/com/fuzzyapps/gustoboliviano/profileFragment.java
@@ -2,10 +2,14 @@ package com.fuzzyapps.gustoboliviano;
 
 
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,7 +18,7 @@ public class profileFragment extends Fragment {
 
 
     private View view;
-
+    LayoutInflater layoutInflater;
     public profileFragment() {
         // Required empty public constructor
     }
@@ -24,7 +28,25 @@ public class profileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.profile_fragment, container, false);
-
+        layoutInflater = getActivity().getLayoutInflater();
+        displayAlertDialog();
         return view;
+    }
+    private void displayAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        View view= layoutInflater.inflate(R.layout.item_review, null);
+        builder.setView(view);
+        builder.setPositiveButton("Enviar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+            }
+        });
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+                dialog.cancel();
+            }
+        });
+        builder.show();
     }
 }
