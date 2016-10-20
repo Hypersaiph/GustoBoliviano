f4a195ed16c4c8f9ad1c2efc3ad1024ae7638d3b
MyGvs <mygvs.mh@gmail.com>
2016-10-19 22:29:00 -0400

    Reviews a Restaurantes

--- app/src/main/java/com/fuzzyapps/gustoboliviano/fragment_restaurant.java
+++ app/src/main/java/com/fuzzyapps/gustoboliviano/fragment_restaurant.java
@@ -1,17 +1,20 @@
 package com.fuzzyapps.gustoboliviano;
 
 import android.app.Fragment;
-import android.content.Intent;
-import android.net.Uri;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -20,11 +23,19 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
 
 public class fragment_restaurant extends Fragment implements OnMapReadyCallback {
+    private LayoutInflater layoutInflater;
     private GoogleMap mMap;
     private Toolbar toolbar;
     public String restaurantName = "Mc Donalds";
+    private DatabaseReference reviewRef;
 
     public fragment_restaurant(){}
     @Override
@@ -35,6 +46,9 @@ public class fragment_restaurant extends Fragment implements OnMapReadyCallback 
     public void onViewCreated(final View view, @Nullable final Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         //AQUI INICIALIZAR LOS OBJETOS
+        layoutInflater = getActivity().getLayoutInflater();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        reviewRef = database.getReference("review");
         final CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) view.findViewById(R.id.collapsingToolbarLayout);
         collapsingToolbarLayout.setTitle("");
         AppBarLayout appBarLayout = (AppBarLayout) view.findViewById(R.id.appBarLayout);
@@ -56,12 +70,16 @@ public class fragment_restaurant extends Fragment implements OnMapReadyCallback 
                 }
             }
         });
-
         MapFragment mapFragment = (MapFragment)  this.getChildFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
+        final FloatingActionButton floatingActionButton = (FloatingActionButton) view.findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayAlertDialog();
+            }
+        });
     }
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -69,5 +87,35 @@ public class fragment_restaurant extends Fragment implements OnMapReadyCallback 
         LatLng LaPaz = new LatLng(-16.499, -68.118);
         mMap.addMarker(new MarkerOptions().position(LaPaz));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(LaPaz.latitude, LaPaz.longitude), 16.0f));
+    }
+    private void displayAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        View view= layoutInflater.inflate(R.layout.item_review, null);
+        builder.setView(view);
+        final RatingBar reviewRatingBar = (RatingBar) view.findViewById(R.id.reviewRatingBar);
+        final EditText reviewTitle = (EditText) view.findViewById(R.id.reviewTitle);
+        final EditText reviewDescription = (EditText) view.findViewById(R.id.reviewDescription);
+        builder.setPositiveButton("Enviar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                writeUserReview(reviewRatingBar.getRating(), reviewTitle.getText().toString(), reviewDescription.getText().toString());
+            }
+        });
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+                dialog.cancel();
+            }
+        });
+        builder.show();
+    }
+    private void writeUserReview(float rating, String title, String description) {
+        double roundRating = (double) Math.round(rating * 100) / 100;
+        Review review = new Review(Globals.userID,Globals.restaurantID, title, description, roundRating, ServerValue.TIMESTAMP);
+        reviewRef.child(Globals.userID).setValue(review, new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                Toast.makeText(getActivity(),"Registrado.",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
